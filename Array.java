8b49b8ade9d3ec0f3d630763803b389719d12d7a
wolf <aswfrln@gmail.com>
2025-08-06 15:36:48 +0000

    Array 1st example

---
+++ Array.java
@@ -0,0 +1,28 @@
+class Array{
+    public static void main(String[] args){
+        int[] arr = new int[5];
+
+        arr[0] = 5;
+        arr[1] = 10;
+        arr[2] = 15;
+
+        System.out.println(arr);
+
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[3]);
+
+        char[] abc = {'q', 'w', 'e', 'r', 't', 'y'};
+
+        System.out.println(abc);
+
+        // System.out.println(abc[0]);
+        // System.out.println(abc[1]);
+        // System.out.println(abc[2]);
+        // System.out.println(abc[3]);
+        // System.out.println(abc[4]);
+        // System.out.println(abc[5]);
+    }
+}
